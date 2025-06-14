72999631f495ef0c08a9db423dd65e9044849948
LilBoiBoi <danialhanif555@outlook.com>
2025-06-14 23:31:57 +0200

    Initial commit

---
+++ src/main/java/com/lilboiboi/fancyname/FancyName.java
@@ -0,0 +1,16 @@
+package com.lilboiboi.fancyname;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FancyName implements ModInitializer {
+	public static final String MOD_ID = "fancyname";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/com/lilboiboi/fancyname/FancyNameClient.java
@@ -0,0 +1,10 @@
+package com.lilboiboi.fancyname;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FancyNameClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
